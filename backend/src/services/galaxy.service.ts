3b62116d56d52b6a6380089c703bb4063cda1169
Hernan-Puschiasis <hernan62002@gmail.com>
2023-10-10 16:32:36 -0300

    Arreglando merges

---
+++ backend/src/services/galaxy.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { World } from '../entities/world.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { CreateWorldDto } from '../dtos/CreateWorldDto';
+import { Galaxy } from '../entities/galaxy.entity';
+import { CreateGalaxyDto } from '../dtos/CreateGalaxyDto';
+
+@Injectable()
+export class GalaxyService {
+  constructor(
+    @InjectRepository(Galaxy)
+    private readonly galaxyRepository: Repository<Galaxy>,
+  ) {}
+
+  async createGalaxy(createGalaxyDto: CreateGalaxyDto): Promise<Galaxy> {
+    const existingGalaxy = await this.galaxyRepository.findOneBy({
+      index: createGalaxyDto.index,
+    });
+    if (existingGalaxy) {
+      throw new Error('Galaxy already exists');
+    }
+    const newGalaxy = this.galaxyRepository.create(createGalaxyDto);
+    return this.galaxyRepository.save(newGalaxy);
+  }
+
+  async getGalaxy(id): Promise<Galaxy> {
+    const galaxy = await this.galaxyRepository.findOne({ where: { id } });
+    if (!galaxy) {
+      throw new Error('Galaxy not found');
+    }
+    return galaxy;
+  }
+
+  async getGalaxies(): Promise<Galaxy[]> {
+    const galaxies = await this.galaxyRepository.find({});
+    if(!galaxies){
+        throw new Error('Galaxies not found');
+    }
+    return galaxies;
+  }
+}
