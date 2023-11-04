27022aa0d1ae2e8e7590f7fbc0b6155f2b749c87
Hernan-Puschiasis <hernan62002@gmail.com>
2023-11-03 23:26:25 -0300

    Arreglado el boton de comenzar duelo y los de seleccionar numero de rondas

--- frontend/src/components/mainDuelScreen/DuelSelect.tsx
+++ frontend/src/components/mainDuelScreen/DuelSelect.tsx
@@ -14,16 +14,20 @@ const DuelSelect: React.FC<DuelSelectProps> = ({onClick, setRoundNumber, setDiff
   const [isDataMissing, setIsDataMissing] = useState(false);
   const options = ['1 Primaria', '2 Primaria', '3 Primaria', '4 Primaria', '5 Primaria', '6 Primaria', '1 Secundaria', '2 Secundaria', '3 Secundaria', '4 Secundaria', '5 Secundaria', '6 Secundaria', 'GAL1 Universidad', 'GAL2 Universidad', 'AM1 Universidad', 'AM2 Universidad','AM3 Universidad', 'PyE Universidad'];
   const [selectedOption, setSelectedOption] = useState('');
+  const [isClickedArray, setIsClickedArray] = useState<boolean[]>([false,false,false]);
   
-  
+  const handleRoundDivClick = (round:number, index:number) => {
+    setRoundNumber(round);
+    setIsClickedArray((prev) => prev.map((_, i) => i === index));
+  }
 
   return (
       <div className='select-container'>
         <p className='room-title'>Crea una sala</p>
         <p className='duel-select-sub-p'>Número de rondas</p>
         <div className='round-container'>
-            {roundNumbers.map((round) => (
-                <div className='round-div' onClick={() => setRoundNumber(round)}>{round}</div>
+            {roundNumbers.map((round, index) => (
+                <div className={`round-div ${isClickedArray[index] ? 'selected-round' : ''}`} onClick={() => handleRoundDivClick(round, index)}>{round}</div>
             ))}
         </div>
         <p className='duel-select-sub-p'>Elige una dificultad</p>
